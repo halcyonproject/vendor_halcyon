027e3d5a8013e487f5dc1ce75ad96f25bb5df942
Aaron Kling <webgeek1234@gmail.com>
2026-04-18 20:11:06 -0500

    kernel: Support having kernel platform in the main repo manifest
    This requires two changes:
    * Block the directory containing these trees from being scanned by bp
    * Parse the full manifest and trim it down to only the relevant kernel
      platform repos for use by the bazel build system to generate module
      versions
    
    Change-Id: I2cb1d2852a5ce7c8e67ca96df01ab50fb720250c
    Signed-off-by: HeroBuxx <me@herobuxx.me>

--- config/common.mk
+++ config/common.mk
@@ -6,6 +6,9 @@ $(call inherit-product-if-exists, external/google-fonts/google-sans-flex/fonts.m
 $(call inherit-product-if-exists, external/google-fonts/lato/fonts.mk)
 $(call inherit-product-if-exists, external/google-fonts/rubik/fonts.mk)
 
+# Exclude kernel platform repos from bp scanning
+PRODUCT_SOURCE_ROOT_DIRS += -kernel/platform
+
 PRODUCT_BRAND ?= Halcyon
 
 PRODUCT_SIZE ?= full
